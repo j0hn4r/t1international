54ce37bf44b7c97da8311b78ce2dd2a2af1d48b6
Richard Peters <richyp@bu.edu>
2016-04-19 17:13:47 +0100

    Add column layout widths

--- apps/pages/layouts.py
+++ apps/pages/layouts.py
@@ -15,6 +15,8 @@ class SingleColumn(PageLayout):
 @templates.attach('glitter_pages.Page')
 class Home(PageLayout):
     billboard_top = columns.Column(width=1080)
+    intro_half_one = columns.Column(width=480)
+    intro_half_two = columns.Column(width=480)
     content = columns.Column(width=960)
     footer = columns.Column(width=960)
 
