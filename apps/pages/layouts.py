2614bee6051afbc36514b089565142c857567b8c
Richard Peters <richyp@bu.edu>
2016-04-19 15:58:32 +0100

    Add templates

--- apps/pages/layouts.py
+++ apps/pages/layouts.py
@@ -5,5 +5,22 @@ from glitter.layouts import PageLayout
 
 
 @templates.attach('glitter_pages.Page')
-class Default(PageLayout):
+class SingleColumn(PageLayout):
+    billboard_top = columns.Column(width=1080)
+    intro = columns.Column(width=960)
     content = columns.Column(width=960)
+    footer = columns.Column(width=960)
+
+
+@templates.attach('glitter_pages.Page')
+class Home(PageLayout):
+    billboard_top = columns.Column(width=1080)
+    content = columns.Column(width=960)
+    footer = columns.Column(width=960)
+
+
+@templates.attach('glitter_pages.Page')
+class TwoColumn(PageLayout):
+    content = columns.Column(width=960)
+    sidebar = columns.Column(width=480)
+    footer = columns.Column(width=960)
