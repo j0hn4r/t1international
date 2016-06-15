72729d0af81051b295697ea184ef67e634947b5f
Richard Peters <richyp@bu.edu>
2016-06-15 13:18:30 +0100

    Add python compatiblility, if statement on marquee block

--- apps/signees/models.py
+++ apps/signees/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Signee(models.Model):
     name = models.CharField(max_length=100)
 
